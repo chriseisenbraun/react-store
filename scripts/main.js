964081f42ba597a2aba49cb29dc3c60d8c7cb900
Chris Eisenbraun <chris@chriseisenbraun.com>
2016-07-11 23:18:14 -0700

    Let add the rest of base files

---
+++ scripts/main.js
@@ -0,0 +1,94 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+/*
+ App
+
+ */
+
+var App = React.createClass({
+
+  render: function() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header />
+        </div>
+        <Order  />
+        <Inventory />
+      </div>
+    );
+  }
+
+});
+
+/*
+ Header
+
+ */
+
+var Header = React.createClass({
+
+  render: function() {
+    return (
+      <p>Header</p>
+    );
+  }
+
+});
+
+/*
+ Order
+
+ */
+
+var Order = React.createClass({
+
+  render: function() {
+    return (
+      <p>Order</p>
+    );
+  }
+
+});
+
+/*
+ Inventory
+
+ */
+
+var Inventory = React.createClass({
+
+  render: function() {
+    return (
+      <p>Inventory</p>
+    );
+  }
+
+});
+
+
+/*
+  Store Picker
+  This will allow us to create a component
+*/
+
+
+var StorePicker = React.createClass({
+
+ render: function() {
+
+   return (
+     <form className="store-selector">
+       <h2>Please Enter a Store</h2>
+       <input type="text" ref="storeId" />
+       <input type="submit" />
+     </form>
+   );
+ }
+
+});
+
+ReactDOM.render(<App/>, document.querySelector('#main'));
+
+// module.exports = storePicker;
