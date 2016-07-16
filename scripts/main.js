0686a1aa6ec422d7d091ef18e6b4ded823160aa5
Chris Eisenbraun <chris@chriseisenbraun.com>
2016-07-15 22:43:34 -0700

    Add Router and components

--- scripts/main.js
+++ scripts/main.js
@@ -1,94 +1,117 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-/*
- App
+var ReactRouter = require('react-router');
+var Router  = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Navigation = ReactRouter.Navigation;
+var createBrowserHistory = require('history/lib/createBrowserHistory');
 
- */
+var h = require('./helpers');
+/*
+  App
+*/
 
 var App = React.createClass({
 
-  render: function() {
+  render : function() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header />
+          <Header tagline="Fresh Seafood Market" />
         </div>
-        <Order  />
-        <Inventory />
+        <Order/>
+        <Inventory/>
       </div>
-    );
+    )
   }
-
 });
 
 /*
- Header
-
- */
-
+  Header
+  <Header/>
+*/
 var Header = React.createClass({
-
-  render: function() {
+  render : function() {
     return (
-      <p>Header</p>
-    );
+      <header className="top">
+        <h1>Catch
+          <span className="ofThe">
+            <span className="of">of</span>
+            <span className="the">the</span>
+          </span>
+          Day</h1>
+        <h3 className="tagline"><span>{this.props.tagline}</span></h3>
+      </header>
+    )
   }
-
-});
+})
 
 /*
- Order
-
- */
-
+  Order
+  <Order/>
+*/
 var Order = React.createClass({
-
-  render: function() {
+  render : function() {
     return (
       <p>Order</p>
-    );
+    )
   }
-
-});
+})
 
 /*
- Inventory
-
- */
-
+  Inventory
+  <Inventory/>
+*/
 var Inventory = React.createClass({
-
-  render: function() {
+  render : function() {
     return (
       <p>Inventory</p>
-    );
+    )
   }
-
-});
+})
 
 
 /*
-  Store Picker
-  This will allow us to create a component
+  StorePicker
+  This will let us make <StorePicker/>
 */
-
 
 var StorePicker = React.createClass({
 
- render: function() {
-
-   return (
-     <form className="store-selector">
-       <h2>Please Enter a Store</h2>
-       <input type="text" ref="storeId" />
-       <input type="submit" />
-     </form>
-   );
- }
+  render : function() {
+    return (
+      <form className="store-selector">
+        <h2>Please Enter A Store</h2>
+        <input type="text" ref="storeId" defaultValue={h.getFunName()} required />
+        <input type="Submit" />
+      </form>
+    )
+  }
 
 });
 
-ReactDOM.render(<App/>, document.querySelector('#main'));
+/*
+  Not Found
+*/
 
-// module.exports = storePicker;
+var NotFound = React.createClass({
+  render : function() {
+    return <h1>Not Found!</h1>
+  }
+});
+
+
+/*
+  Routes
+*/
+
+var routes = (
+  <Router history={createBrowserHistory()}>
+    <Route path="/" component={StorePicker}/>
+    <Route path="/store/:storeId" component={App}/>
+    <Route path="*" component={NotFound}/>
+  </Router>
+)
+
+ReactDOM.render(routes, document.querySelector('#main'));
