a26c01c631be34c210e648b7c342b8ddf3906587
Chris Eisenbraun <chris@chriseisenbraun.com>
2016-07-16 17:02:28 -0700

    Adding event

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ var ReactDOM = require('react-dom');
 var ReactRouter = require('react-router');
 var Router  = ReactRouter.Router;
 var Route = ReactRouter.Route;
-var Navigation = ReactRouter.Navigation;
+var History = ReactRouter.History;
 var createBrowserHistory = require('history/lib/createBrowserHistory');
 
 var h = require('./helpers');
@@ -78,10 +78,16 @@ var Inventory = React.createClass({
 */
 
 var StorePicker = React.createClass({
-
+  mixins : [History],
+  goToStore : function(event) {
+    event.preventDefault();
+    // get the data from the input
+    var storeId = this.refs.storeId.value;
+    this.history.pushState(null, '/store/' + storeId);
+  },
   render : function() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
         <input type="text" ref="storeId" defaultValue={h.getFunName()} required />
         <input type="Submit" />
